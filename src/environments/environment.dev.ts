3daa9623d65301316f43fb4c4ea20e627504b157
Pola Nasser <121816986+POlaNasserSamouel@users.noreply.github.com>
2026-09-26 12:58:39 +0300

    Clean up environment.dev.ts by removing comments
    Removed commented-out production configuration.

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,7 +1,4 @@
 export const environment = {
-/*   production: false,
-  apiBaseUrl: 'https://pinkyauradevapi.ape-org.com/api',
-  imageApiBaseUrl: 'https://pinkyauradevapi.ape-org.com' */
     production: false,
   apiBaseUrl: 'https://localhost:7256/api',
   imageApiBaseUrl: 'https://localhost:7256'
